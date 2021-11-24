57605bdee3cf01a1b9c5e20bbb8a731a4f9ad69c
Анна <82878523+Nedololli@users.noreply.github.com>
2021-11-24 16:13:07 +0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,28 +1,47 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
+
+
+def sumColor():
+    global n, n1, sum_color
+    for n in range(i, i + size):
+        sum_color += np.sum(arr[n][j:j+size][:])
+    sum_color //= 100
+
+def grayColor():
+    global n, n1
+    for n in range(i, i + size):
+        arr[n][j:j+size][:] = int(sum_color // 50) * 50 // gray
+
+
+
+UrlImage = input(" укажите название файла, файл должен находится в корне. >> ")
+UrlImage = UrlImage + ".jpg"
+img = Image.open(UrlImage)
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+size = int(input("Введите размер ячейки >> "))
+
+while True:
+    try:
+        switch = input(" Введите режим градации серого (4/6) >> ")
+        if(switch == "4" or switch == "6"):
+            gray = int(switch)
+            break
+        raise("Только 4/6")
+    except:
+        print("Только 4/6")
+lenght_pic = len(arr)
 i = 0
-while i < a - 11:
+
+
+
+while i < lenght_pic - 1:
     j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+    while j < lenght_pic - 1:
+        sum_color = 0
+        sumColor()
+        grayColor()
+        j = j + size
+    i = i + size
 res = Image.fromarray(arr)
 res.save('res.jpg')
